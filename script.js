7e4d3a2925ca23d4e19c2b0cd21e1a168f369520
Julien Baouz <baouzjulien@gmail.com>
2025-10-19 19:12:28 +0200

    Add quiz questions and related functions

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+const questions = [
+  {
+  category: "Animaux",
+  question: "Quel race de chat provient de l'ancienne Thaïlande ?",
+  choices: ["Siamois", "Tigré", "Persan"],
+  answer: "Siamois"
+  }, 
+  {
+  category: "Histoire",
+  question: "En quelle année a démarrée la première guerre mondiale ?",
+  choices: ["1900", "2000", "1914"],
+  answer: "1914"
+  }, {
+  category: "Sport",
+  question: "Quelle course trouve son nom dans une bataille de la Grèce antique et se déroule sur une distance d'environ 40 km ?",
+  choices: ["Sprint", "Marathon", "110m haie"],
+  answer: "Marathon" 
+  }, {
+  category: "Musique",
+  question: "Quel groupe mythique des années 1960 a composé l'album 'The Wall' ?",
+  choices: ["Nirvana", "The Beatles", "Pink Floyd"],
+  answer: "Pink Floyd"
+  }, {
+  category: "Politique",
+  question: "Quelle président Français a fait campagne sur le slogan de 'mangez des pommes' ?",
+  choices: ["Mittérand", "Sarkozy", "Chirac"],
+  answer: "Chirac"
+  }, 
+];
 
+const getRandomQuestion = (questionsArray) => {
+  return questions[Math.floor(Math.random() * questionsArray.length)];
+};
+
+const getRandomComputerChoice = (availableChoicesArray) => {
+  return availableChoicesArray[Math.floor(Math.random() * availableChoicesArray.length)];
+};
+
+const getResults = (questionObject, computerChoice) => {
+  if(questionObject.answer === computerChoice) {
+    return "The computer's choice is correct!";
+  } else {
+    return `The computer's choice is wrong. The correct answer is: ${questionObject.answer}`
+  };
+};
